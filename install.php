080985a07d995d7344fb5f78660dc046e7be578d
bertrandfongang <bertrandfongang>
2021-12-15 21:55:44 +0100

    complete

--- install.php
+++ install.php
@@ -3,7 +3,7 @@
 
 // Set variables for our request
 $api_key = "e97e2bd0a49fb36acbd19e4f7a9fb842"; //Replace with your API KEY
-$shared_secret = "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"; //Replace with your Shared secret key
+$shared_secret = "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"; //Replace with your Shared secret key
 $params = $_GET; // Retrieve all request parameters
 $hmac = $_GET['hmac']; // Retrieve HMAC request parameter
 
