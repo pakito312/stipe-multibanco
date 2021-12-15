de0361156b5f4d20cd20f2e5d34e9a63a4a800d3
bertrandfongang <bertrandfongang>
2021-12-15 21:17:09 +0100

    complete

--- install.php
+++ install.php
@@ -1,6 +1,5 @@
 <?php
 // Get our helper functions
-require_once("config.php");
 
 // Set variables for our request
 $api_key = "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"; //Replace with your API KEY
@@ -17,40 +16,37 @@ $computed_hmac = hash_hmac('sha256', http_build_query($params), $shared_secret);
 
 // Use hmac data to check that the response is from Shopify or not
 if (hash_equals($hmac, $computed_hmac)) {
+    try {
+        // Set variables for our request
+        $query = array(
+            "client_id" => $api_key, // Your API key
+            "client_secret" => $shared_secret, // Your app credentials (secret key)
+            "code" => $params['code'] // Grab the access key from the URL
+        );
 
-	// Set variables for our request
-	$query = array(
-		"client_id" => $api_key, // Your API key
-		"client_secret" => $shared_secret, // Your app credentials (secret key)
-		"code" => $params['code'] // Grab the access key from the URL
-	);
+        // Generate access token URL
+        $access_token_url = "https://" . $params['shop'] . "/admin/oauth/access_token";
 
-	// Generate access token URL
-	$access_token_url = "https://" . $params['shop'] . "/admin/oauth/access_token";
+        // Configure curl client and execute request
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $access_token_url);
+        curl_setopt($ch, CURLOPT_POST, count($query));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($query));
+        $result = curl_exec($ch);
+        curl_close($ch);
 
-	// Configure curl client and execute request
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_URL, $access_token_url);
-	curl_setopt($ch, CURLOPT_POST, count($query));
-	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($query));
-	$result = curl_exec($ch);
-	curl_close($ch);
-
-	// Store the access token
-	$result = json_decode($result, true);
-	$access_token = $result['access_token'];
-
-	// Show the access token (don't do this in production!)
-	//echo $access_token;
-	if( $access_token) {
-		header('Location: https://' . $shop_url . '/admin/apps');
-		exit();
-	} else {
-		echo "Error installation: " . mysqli_error( $conn );
-	}
-
+        // Store the access token
+        $result = json_decode($result, true);
+        $access_token = $result['access_token'];
+        if ($access_token) {
+            header('Location: https://' . $shop_url . '/admin/apps');
+            exit();
+        }
+    } catch (\Throwable $th) {
+        echo "Error installation: " . $th->getMessage();
+    }
 } else {
-	// Someone is trying to be shady!
-	die('This request is NOT from Shopify!');
+    // Someone is trying to be shady!
+    die('This request is NOT from Shopify!');
 }
